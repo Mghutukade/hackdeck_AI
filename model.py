e41b839f64cc64693512868e09f55fb5c776b5fa
Mahesh <msghutukade1@gmail.com>
2026-01-28 12:53:28 +0530

    Cleanup before new implementation

--- model.py
+++
@@ -1,9 +0,0 @@
-import os
-import tensorflow as tf
-
-MODEL_PATH = "trained_model.h5"
-
-def load_trained_model():
-    if not os.path.exists(MODEL_PATH):
-        raise FileNotFoundError("❌ Trained model not found. Run train_model.py first.")
-    return tf.keras.models.load_model(MODEL_PATH)
